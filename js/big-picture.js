6fcdd9d47f121a3135d715575728300fe7d27bc6
Александр Смирнов <smirnovalexander.cat@gmail.com>
2024-07-18 15:03:17 +0300

    feat: 8.15. Открывается и закрывается (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -5,17 +5,35 @@ const BIG_PICTURE_POPUP = document.querySelector('.big-picture');
 const BIG_PICTURE = BIG_PICTURE_POPUP.querySelector('.big-picture__img img');
 const BIG_PICTURE_DESCRIPTION = BIG_PICTURE_POPUP.querySelector('.social__caption');
 const LIKES_COUNT = BIG_PICTURE_POPUP.querySelector('.likes-count');
-const COMMENT_COUNT_HEADER = BIG_PICTURE_POPUP.querySelector('.social__comment-count');
 const COMMENT_SHOW_COUNT = BIG_PICTURE_POPUP.querySelector('.social__comment-shown-count');
 const COMMENT_ALL_COUNT = BIG_PICTURE_POPUP.querySelector('.social__comment-total-count');
 const COMMENTS_LIST = BIG_PICTURE_POPUP.querySelector('.social__comments');
 const COMMENT_MORE_LOAD_BUTTON = BIG_PICTURE_POPUP.querySelector('.comments-loader');
 const CLOSE_POPUP_BUTTON = BIG_PICTURE_POPUP.querySelector('.big-picture__cancel');
 
+const MAX_COMMENT_SHOW_COUNT = 5;
+const STEP_OPEN_MORE_COMMENT = 5;
+
+let maxCommentShow = MAX_COMMENT_SHOW_COUNT;
+
+const showMoreComment = () => {
+  BIG_PICTURE_POPUP.querySelectorAll('.social__comment').forEach((elem, index) => {
+    elem.classList.remove('hidden');
+
+    if((index + 1) > maxCommentShow) {
+      elem.classList.add('hidden');
+    }
+  });
+
+  COMMENT_SHOW_COUNT.textContent = BIG_PICTURE_POPUP.querySelectorAll('.social__comment:not(.hidden)').length;
+};
+
 const closePopupKeydown = (evt) => {
   if(evt.key === 'Escape') {
     document.body.classList.remove('modal-open');
     BIG_PICTURE_POPUP.classList.add('hidden');
+
+    maxCommentShow = MAX_COMMENT_SHOW_COUNT;
 
     document.removeEventListener('keydown', closePopupKeydown);
   }
@@ -33,6 +51,8 @@ const closePopup = () => {
   document.body.classList.remove('modal-open');
   BIG_PICTURE_POPUP.classList.add('hidden');
 
+  maxCommentShow = MAX_COMMENT_SHOW_COUNT;
+
   document.removeEventListener('keydown', closePopupKeydown);
 };
 
@@ -46,10 +66,6 @@ const bigPicturePopupInit = (pictureData) => {
       BIG_PICTURE.src = THUMBNAIL.querySelector('.picture__img').src;
       BIG_PICTURE_DESCRIPTION.textContent = THUMBNAIL.querySelector('.picture__img').alt;
       LIKES_COUNT.textContent = THUMBNAIL.querySelector('.picture__likes').textContent;
-      COMMENT_COUNT_HEADER.classList.add('hidden');
-      COMMENT_SHOW_COUNT.textContent = BIG_PICTURE_POPUP.querySelectorAll('.social__comment').length;
-      COMMENT_ALL_COUNT.textContent = THUMBNAIL.querySelector('.picture__comments').textContent;
-      COMMENT_MORE_LOAD_BUTTON.classList.add('hidden');
 
       COMMENTS_LIST.innerHTML = '';
       pictureData.forEach(({url, comments}) => {
@@ -59,6 +75,16 @@ const bigPicturePopupInit = (pictureData) => {
           });
         }
       });
+
+      showMoreComment();
+
+      COMMENT_MORE_LOAD_BUTTON.addEventListener('click', () => {
+        maxCommentShow += STEP_OPEN_MORE_COMMENT;
+
+        showMoreComment();
+      });
+
+      COMMENT_ALL_COUNT.textContent = THUMBNAIL.querySelector('.picture__comments').textContent;
     }
   });
 
